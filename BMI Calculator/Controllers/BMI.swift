4fed43a7c8498000c86692ac6d5276da12eb1181
Merrick McDougal <Merrick@mrmcdougal.com>
2021-03-24 02:19:40 -0500

    Final Product

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Merrick Mcdougal on 3/3/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
